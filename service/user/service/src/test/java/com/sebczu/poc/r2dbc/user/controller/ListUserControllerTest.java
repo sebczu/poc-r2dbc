4ba78301ae1ade56ec452c90b0c7092056166452
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-09-20 23:12:50 +0200

    use testcontainer dependency

--- service/user/service/src/test/java/com/sebczu/poc/r2dbc/user/controller/ListUserControllerTest.java
+++ service/user/service/src/test/java/com/sebczu/poc/r2dbc/user/controller/ListUserControllerTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 
 class ListUserControllerTest extends UserControllerTest {
 
--- service/user/service/src/test/java/com/sebczu/poc/r2dbc/user/controller/UserControllerTest.java
+++ service/user/service/src/test/java/com/sebczu/poc/r2dbc/user/controller/UserControllerTest.java
@@ -2,21 +2,32 @@ package com.sebczu.poc.r2dbc.user.controller;
 
 import com.sebczu.poc.r2dbc.user.repository.UserRepository;
 import com.sebczu.poc.r2dbc.user.repository.entity.UserEntity;
-import lombok.extern.slf4j.Slf4j;
+import io.r2dbc.spi.ConnectionFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
+import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
+import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
 import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.PostgreSQLContainer;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
 
-@Slf4j
 @SpringBootTest
+@ContextConfiguration(initializers = {UserControllerTest.ContextInitializer.class})
 @AutoConfigureWebTestClient
 @EnableR2dbcRepositories(basePackages = "com.sebczu")
 abstract class UserControllerTest {
@@ -34,7 +45,7 @@ abstract class UserControllerTest {
 
     @BeforeEach
     @AfterEach
-    public void cleanRepository() {
+    private void cleanRepository() {
         databaseClient.delete().from(UserEntity.class)
                 .fetch()
                 .rowsUpdated()
@@ -51,5 +62,38 @@ abstract class UserControllerTest {
     protected Flux<UserEntity> getAllUsers() {
         return databaseClient.select().from(UserEntity.class)
                 .fetch().all();
+    }
+
+    static class ContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:13")
+                    .withDatabaseName("test")
+                    .withUsername("admin")
+                    .withPassword("test");
+
+            postgreSQLContainer.start();
+
+            TestPropertyValues.of(
+                    "spring.r2dbc.url=" + "r2dbc:postgresql://" + postgreSQLContainer.getHost() + ":" + postgreSQLContainer.getFirstMappedPort() + "/" + postgreSQLContainer.getDatabaseName(),
+                    "spring.r2dbc.username=" + postgreSQLContainer.getUsername(),
+                    "spring.r2dbc.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(applicationContext);
+        }
+    }
+
+    @TestConfiguration
+    static class TestInitalizer {
+        @Bean
+        public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+            ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+            initializer.setConnectionFactory(connectionFactory);
+
+            CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+            populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+            initializer.setDatabasePopulator(populator);
+
+            return initializer;
+        }
     }
 }
