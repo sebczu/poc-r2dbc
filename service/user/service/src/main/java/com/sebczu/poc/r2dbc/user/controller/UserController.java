55e44cc0503a9ec7027adee6f2bad611743c971c
sebastian.czubala.dev <sebastian.czubala.dev@gmail.com>
2020-09-20 09:27:38 +0200

    add logger for controller

--- service/user/service/src/main/java/com/sebczu/poc/r2dbc/user/controller/UserController.java
+++ service/user/service/src/main/java/com/sebczu/poc/r2dbc/user/controller/UserController.java
@@ -5,12 +5,14 @@ import com.sebczu.poc.r2dbc.user.domain.User;
 import com.sebczu.poc.r2dbc.user.repository.UserRepository;
 import com.sebczu.poc.r2dbc.user.repository.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController implements UserAPI {
@@ -18,31 +20,40 @@ public class UserController implements UserAPI {
     private final UserRepository repository;
 
     public Flux<User> list() {
+        log.info("list users");
         return repository.findAll()
                 .delayElements(Duration.ofMillis(100))
-                .map(entity -> new User(entity.getId(), entity.getName()));
+                .map(entity -> new User(entity.getId(), entity.getName()))
+                .log();
     }
 
     public Mono<User> get(Integer id) {
+        log.info("get user {}", id);
         return repository.findById(id)
-                .map(entity -> new User(entity.getId(), entity.getName()));
+                .map(entity -> new User(entity.getId(), entity.getName()))
+                .log();
     }
 
     public Mono<User> create(User user) {
+        log.info("create user {}", user);
         return Mono.just(user)
                 .map(user1 -> new UserEntity(user1.getName()))
                 .flatMap(repository::save)
-                .map(entity -> new User(entity.getId(), entity.getName()));
+                .map(entity -> new User(entity.getId(), entity.getName()))
+                .log();
     }
 
     public Mono<User> update(Integer id, User user) {
+        log.info("update user {} ({})", id, user);
         return Mono.just(user)
                 .map(user1 -> new UserEntity(id, user1.getName()))
                 .flatMap(repository::save)
-                .map(entity -> new User(id, entity.getName()));
+                .map(entity -> new User(id, entity.getName()))
+                .log();
     }
 
     public Mono<Void> delete(Integer id) {
-        return repository.deleteById(id);
+        log.info("delete user {}", id);
+        return repository.deleteById(id).log();
     }
 }
